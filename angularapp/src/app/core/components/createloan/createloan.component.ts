229e99ac3f010d9ccb2f59bffca72cd11d455d82
mohdzuhaib10 <mohammedzuhaib2410@gmail.com>
2025-11-01 20:10:57 +0530

    Two-Way Binding for Create Loan Form

--- angularapp/src/app/core/components/createloan/createloan.component.ts
+++ angularapp/src/app/core/components/createloan/createloan.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Loan } from '../../models/loan.model';
 
 @Component({
   selector: 'app-createloan',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./createloan.component.css']
 })
 export class CreateloanComponent {
+  model: Loan;
 
+  constructor() {
+    this.model = {
+      loanType: '',
+      description: '',
+      interestRate: 0,
+      maximumAmount: 0
+    }
+  }
+
+  createLoan() {
+
+  }
 }
---
+++ angularapp/src/app/core/models/loan.model.ts
@@ -0,0 +1,6 @@
+export interface Loan {
+    loanType: string;
+    description: string;
+    interestRate: number;
+    maximumAmount: number;
+}
