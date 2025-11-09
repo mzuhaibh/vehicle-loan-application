288025cecb645615d0393d397afd6929008a0da6
mohdzuhaib10 <mohammedzuhaib2410@gmail.com>
2025-11-09 16:20:08 +0530

    Remove DTO

--- dotnetapp/Controllers/LoanController.cs
+++ dotnetapp/Controllers/LoanController.cs
@@ -1,6 +1,5 @@
 ﻿using dotnetapp.Data;
 using dotnetapp.Models.Domain;
-using dotnetapp.Models.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,16 +16,8 @@ namespace dotnetapp.Controllers
         }
 
         [HttpPost("/api/loan")]
-        public async Task<IActionResult> CreateLoan(LoanRequestDto request)
+        public async Task<IActionResult> CreateLoan(Loan loan)
         {
-            var loan = new Loan
-            {
-                LoanType = request.LoanType,
-                Description = request.Description,
-                InterestRate = request.InterestRate,
-                MaximumAmount = request.MaximumAmount
-            };
-
             bool result = await loanService.CreateAsync(loan);
 
             if(result)
@@ -40,20 +31,7 @@ namespace dotnetapp.Controllers
         [HttpGet("/api/loans")]
         public async Task<IActionResult> GetAllLoans()
         {
-            var loans = await loanService.GetAllAsync();
-            var response = new List<LoanDto>();
-
-            foreach(var loan in loans)
-            {
-                response.Add(new LoanDto
-                {
-                    LoanId = loan.LoanId,
-                    LoanType = loan.LoanType,
-                    Description = loan.Description,
-                    InterestRate = loan.InterestRate,
-                    MaximumAmount = loan.MaximumAmount
-                });
-            }
+            var response = await loanService.GetAllAsync();
 
             return Ok(response);
         }
@@ -61,37 +39,19 @@ namespace dotnetapp.Controllers
         [HttpGet("/api/loans/{loanId}")]
         public async Task<IActionResult> GetLoanById(int loanId)
         {
-            var loan = await loanService.GetByIdAsync(loanId);
+            var response = await loanService.GetByIdAsync(loanId);
 
-            if(loan  == null)
+            if(response == null)
             {
                 return NotFound("Loan not found!");
             }
-
-            var response = new LoanDto
-            {
-                LoanId = loan.LoanId,
-                LoanType = loan.LoanType,
-                Description = loan.Description,
-                InterestRate = loan.InterestRate,
-                MaximumAmount = loan.MaximumAmount,
-            };
 
             return Ok(response);
         }
 
         [HttpPut("/api/loan")]
-        public async Task<IActionResult> UpdateLoan(LoanRequestDto request)
+        public async Task<IActionResult> UpdateLoan(Loan loan)
         {
-            var loan = new Loan
-            {
-                LoanId = request.LoanId,
-                LoanType = request.LoanType,
-                Description = request.Description,
-                InterestRate = request.InterestRate,
-                MaximumAmount = request.MaximumAmount
-            };
-
             bool result = await loanService.UpdateAsync(loan);
 
             if(result)
